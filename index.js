a26413d22ef56bb13de37cd9a85a2e8f596fc037
Daniel <>
2021-03-01 10:31:43 -0500

    Refactor commands; add !eval

--- index.js
+++ index.js
@@ -16,8 +16,33 @@ client.on('message', msg => {
 
 client.on('message', message => {
   msg = message.content.split(' ');
-  if (msg.length <= 1 || msg[0] !== '!ram') return;
-  message.channel.send(msg.slice(1).join(' '));
-})
+  switch (msg[0]) {
+    case '!ram':
+      repeatAfterMe(msg, message);
+      break;
+    case '!eval':
+      evaluateString(msg, message);
+      break;
+  }
+});
+
+function repeatAfterMe(content, msg) {
+  msg.channel.send(content.slice(1).join(' '));
+}
+
+function evaluateString(content, msg) {
+  if (msg.member.id !== '669717327293710337') return;
+  try {
+    const cons = {
+      log: arg => msg.channel.send(arg, {split: true})
+    };
+    const res = eval(`((console) => {${content.slice(1).join(' ').replace(/```([\s\S]+?)```/, '$1')}})`)(cons);
+    if (res !== undefined) {
+      cons.log(res);
+    }
+  } catch (e) {
+    msg.channel.send('Error: ' + e.message, {split: true});
+  }
+}
 
 client.on('error', console.log);
